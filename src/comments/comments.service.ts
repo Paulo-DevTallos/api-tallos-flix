cfdebb7f34b7f336faefa17d5b0d04813ed6c8ac
Paulo Sergio <pauloserg861@gmail.com>
2022-09-02 15:54:44 -0300

    ajustando tipagem / implementando busca por id

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -3,11 +3,12 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { Comments } from './entities/comment.entity';
 
 @Injectable()
 export class CommentsService {
   constructor(
-    @InjectModel(Comment.name)
+    @InjectModel(Comments.name)
     private readonly commentsModel: Model<CreateCommentDto>,
   ) {}
 
@@ -19,15 +20,15 @@ export class CommentsService {
     return this.commentsModel.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} comment`;
+  findOne(id: string) {
+    return this.commentsModel.findById({ _id: id });
   }
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
+  update(id: string, updateCommentDto: UpdateCommentDto) {
     return `This action updates a #${id} comment`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} comment`;
   }
 }
