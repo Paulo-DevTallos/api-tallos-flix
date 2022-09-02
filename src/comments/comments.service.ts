069f9112f8336886afe6e5693da7f9acded88017
Paulo Sergio <pauloserg861@gmail.com>
2022-09-02 15:43:40 -0300

    implementando busca e configurando verbo GET

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Injectable()
 export class CommentsService {
+  constructor(
+    @InjectModel(Comment.name)
+    private readonly commentsModel: Model<CreateCommentDto>,
+  ) {}
+
   create(createCommentDto: CreateCommentDto) {
     return 'This action adds a new comment';
   }
 
   findAll() {
-    return `This action returns all comments`;
+    return this.commentsModel.find();
   }
 
   findOne(id: number) {
