9b6ea7eae09c9037082f1a034fbf9837980cd49d
Paulo Sergio <pauloserg861@gmail.com>
2022-09-02 15:35:17 -0300

    configurando collection no modulo de comentarios

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
 import { MongooseModule } from '@nestjs/mongoose';
-import { CommentSchema } from './entities/comment.entity';
+import { Comments, CommentsSchema } from './entities/comment.entity';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
+    MongooseModule.forFeature([
+      { name: Comments.name, schema: CommentsSchema },
+    ]),
   ],
   controllers: [CommentsController],
   providers: [CommentsService],
