ec6e739a6215c6ad9e3eb64001dc5bec4d872906
Paulo Sergio <pauloserg861@gmail.com>
2022-09-05 14:49:18 -0300

    implemanetando metodo de listagem geral

--- src/sessions/sessions.service.ts
+++ src/sessions/sessions.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateSessionDto } from './dto/create-session.dto';
 import { UpdateSessionDto } from './dto/update-session.dto';
+import { Session } from './entities/session.entity';
 
 @Injectable()
 export class SessionsService {
+  constructor(
+    @InjectModel(Session.name)
+    private readonly sessionModel: Model<CreateSessionDto>,
+  ) {}
+
   create(createSessionDto: CreateSessionDto) {
     return 'This action adds a new session';
   }
 
   findAll() {
-    return `This action returns all sessions`;
+    return this.sessionModel.find();
   }
 
   findOne(id: number) {
