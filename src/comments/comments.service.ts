690779546ae376d791e2cc3832f6d90f811129dd
Paulo Dev Tallos <107865369+Paulo-DevTallos@users.noreply.github.com>
2022-09-05 15:05:57 -0300

    Revert "ajustando tipagem / implementando busca por id"
    This reverts commit df2335c162a368373506192895c895aa58178d2c.

--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -3,12 +3,11 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
-import { Comments } from './entities/comment.entity';
 
 @Injectable()
 export class CommentsService {
   constructor(
-    @InjectModel(Comments.name)
+    @InjectModel(Comment.name)
     private readonly commentsModel: Model<CreateCommentDto>,
   ) {}
 
@@ -20,15 +19,15 @@ export class CommentsService {
     return this.commentsModel.find();
   }
 
-  findOne(id: string) {
-    return this.commentsModel.findById({ _id: id });
+  findOne(id: number) {
+    return `This action returns a #${id} comment`;
   }
 
-  update(id: string, updateCommentDto: UpdateCommentDto) {
+  update(id: number, updateCommentDto: UpdateCommentDto) {
     return `This action updates a #${id} comment`;
   }
 
-  remove(id: string) {
+  remove(id: number) {
     return `This action removes a #${id} comment`;
   }
 }
