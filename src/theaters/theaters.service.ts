8f1eeb8f3f2438c03f6dd06064f63d62e1173904
Paulo Dev Tallos <107865369+Paulo-DevTallos@users.noreply.github.com>
2022-09-05 15:05:57 -0300

    Revert "implementando metodo de busca geral"
    This reverts commit 5205deff555169810d380e45c8933ae77891c321.

--- src/theaters/theaters.service.ts
+++ src/theaters/theaters.service.ts
@@ -1,23 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import { CreateTheaterDto } from './dto/create-theater.dto';
 import { UpdateTheaterDto } from './dto/update-theater.dto';
-import { Theaters } from './entities/theater.entity';
 
 @Injectable()
 export class TheatersService {
-  constructor(
-    @InjectModel(Theaters.name)
-    private readonly theatersModel: Model<CreateTheaterDto>,
-  ) {}
-
   create(createTheater: CreateTheaterDto) {
     return 'This action adds a new thater';
   }
 
-  async findAll() {
-    return await this.theatersModel.find();
+  findAll() {
+    return `This action returns all thaters`;
   }
 
   findOne(id: string) {
