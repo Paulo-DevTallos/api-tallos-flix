7d57820072275e23708f74d46d3dcff2c7242929
Paulo Dev Tallos <107865369+Paulo-DevTallos@users.noreply.github.com>
2022-09-05 15:05:57 -0300

    Revert "importando movies modules"
    This reverts commit 0e1603e27d8e77584e9a8abe4e8826b73c4516d1.

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 import { CommentsModule } from './comments/comments.module';
-import { MoviesModule } from './movies/movies.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DATA_BASE_STRING_CONNECTION),
     UsersModule,
     CommentsModule,
-    MoviesModule,
   ],
   controllers: [],
   providers: [],
