abb3bc3ab2ca942fea516a5513be184f636b5d3d
Paulo Dev Tallos <107865369+Paulo-DevTallos@users.noreply.github.com>
2022-09-05 15:05:57 -0300

    Revert "implementando busca e configurando verbo GET"
    This reverts commit 069f9112f8336886afe6e5693da7f9acded88017.

--- src/comments/comments.controller.ts
+++ src/comments/comments.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
@@ -20,7 +12,7 @@ export class CommentsController {
     return this.commentsService.create(createCommentDto);
   }
 
-  @Get('GET')
+  @Get()
   findAll() {
     return this.commentsService.findAll();
   }
--- src/comments/comments.service.ts
+++ src/comments/comments.service.ts
@@ -1,22 +1,15 @@
 import { Injectable } from '@nestjs/common';
-import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
 
 @Injectable()
 export class CommentsService {
-  constructor(
-    @InjectModel(Comment.name)
-    private readonly commentsModel: Model<CreateCommentDto>,
-  ) {}
-
   create(createCommentDto: CreateCommentDto) {
     return 'This action adds a new comment';
   }
 
   findAll() {
-    return this.commentsModel.find();
+    return `This action returns all comments`;
   }
 
   findOne(id: number) {
