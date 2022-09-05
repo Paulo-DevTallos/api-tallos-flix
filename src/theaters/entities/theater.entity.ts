671102a952dd21ed3bcbe3eb7a54f4934ca157b2
Paulo Sergio <pauloserg861@gmail.com>
2022-09-05 13:46:01 -0300

    criando entidade de theaters

--- src/theaters/entities/theater.entity.ts
+++ src/theaters/entities/theater.entity.ts
@@ -1,1 +1,26 @@
-export class Theater {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type TheatersDocument = Theaters & Document;
+
+@Schema()
+export class Theaters {
+  @Prop()
+  id: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  email: string;
+
+  @Prop()
+  movie_id: string;
+
+  @Prop()
+  text: string;
+
+  @Prop()
+  date: Date;
+}
+
+export const TheatersSchema = SchemaFactory.createForClass(Theaters);
