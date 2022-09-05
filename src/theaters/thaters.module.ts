5eb069cc7e4dca4a6507934ed90f7ef66d8b46f8
Paulo Sergio <pauloserg861@gmail.com>
2022-09-05 14:14:18 -0300

    implementando metodo de busca geral

--- src/theaters/thaters.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TheatersService } from './theaters.service';
-import { TheatersController } from './theaters.controller';
-
-@Module({
-  controllers: [TheatersController],
-  providers: [TheatersService],
-})
-export class TheatersModule {}
