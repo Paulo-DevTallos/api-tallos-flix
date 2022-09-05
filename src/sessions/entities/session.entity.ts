25ac641aff06d8316c8e2a3f6f00ffdcf5b849a5
Paulo Sergio <pauloserg861@gmail.com>
2022-09-05 14:43:28 -0300

    criando entidade de sessions

--- src/sessions/entities/session.entity.ts
+++ src/sessions/entities/session.entity.ts
@@ -1,1 +1,14 @@
-export class Session {}
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+
+export type SessionDocument = Session & Document;
+
+@Schema()
+export class Session {
+  @Prop()
+  user_id: string;
+
+  @Prop()
+  jwt: string;
+}
+
+export const SessionSchema = SchemaFactory.createForClass(Session);
