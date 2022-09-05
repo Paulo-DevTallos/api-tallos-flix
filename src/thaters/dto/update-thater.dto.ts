aeae987f8456a184fab675260df6fbdc10397376
Paulo Sergio <pauloserg861@gmail.com>
2022-09-05 13:42:57 -0300

    corrigindo nome do resource

--- src/thaters/dto/update-thater.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateThaterDto } from './create-thater.dto';
-
-export class UpdateThaterDto extends PartialType(CreateThaterDto) {}
--- src/theaters/theaters.service.ts
+++ src/theaters/theaters.service.ts
@@ -1,10 +1,10 @@
 import { Injectable } from '@nestjs/common';
-import { CreateThaterDto } from './dto/create-thater.dto';
-import { UpdateThaterDto } from './dto/update-thater.dto';
+import { CreateTheaterDto } from './dto/create-theater.dto';
+import { UpdateTheaterDto } from './dto/update-theater.dto';
 
 @Injectable()
-export class ThatersService {
-  create(createThaterDto: CreateThaterDto) {
+export class TheatersService {
+  create(createTheater: CreateTheaterDto) {
     return 'This action adds a new thater';
   }
 
@@ -12,15 +12,15 @@ export class ThatersService {
     return `This action returns all thaters`;
   }
 
-  findOne(id: number) {
+  findOne(id: string) {
     return `This action returns a #${id} thater`;
   }
 
-  update(id: number, updateThaterDto: UpdateThaterDto) {
+  update(id: string, updateTheaterDto: UpdateTheaterDto) {
     return `This action updates a #${id} thater`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} thater`;
   }
 }
