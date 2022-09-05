5205deff555169810d380e45c8933ae77891c321
Paulo Sergio <pauloserg861@gmail.com>
2022-09-05 14:14:18 -0300

    implementando metodo de busca geral

--- src/theaters/theaters.service.ts
+++ src/theaters/theaters.service.ts
@@ -1,15 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateTheaterDto } from './dto/create-theater.dto';
 import { UpdateTheaterDto } from './dto/update-theater.dto';
+import { Theaters } from './entities/theater.entity';
 
 @Injectable()
 export class TheatersService {
+  constructor(
+    @InjectModel(Theaters.name)
+    private readonly theatersModel: Model<CreateTheaterDto>,
+  ) {}
+
   create(createTheater: CreateTheaterDto) {
     return 'This action adds a new thater';
   }
 
-  findAll() {
-    return `This action returns all thaters`;
+  async findAll() {
+    return await this.theatersModel.find();
   }
 
   findOne(id: string) {
