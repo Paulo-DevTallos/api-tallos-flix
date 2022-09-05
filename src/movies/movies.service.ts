52335208737a039394b8decb97421d0e49595e7c
Paulo Sergio <pauloserg861@gmail.com>
2022-09-05 11:13:29 -0300

    implementando inject Model do DTO

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
 import { CreateMovieDto } from './dto/create-movie.dto';
 import { UpdateMovieDto } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
 
 @Injectable()
 export class MoviesService {
+  constructor(
+    @InjectModel(Movie.name)
+    private readonly moviesModel: Model<CreateMovieDto>,
+  ) {}
+
   create(createMovie: CreateMovieDto) {
     return 'This action adds a new movie';
   }
