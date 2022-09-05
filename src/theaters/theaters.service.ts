a47d5df324ea63ffa795cc91d1aed386a250c8ce
Paulo Dev Tallos <107865369+Paulo-DevTallos@users.noreply.github.com>
2022-09-05 15:05:57 -0300

    Revert "implementando metodos de deleteOne"
    This reverts commit 1ded55844a91e15aa083ea3777dd6aa78083d925.

--- src/theaters/theaters.service.ts
+++ src/theaters/theaters.service.ts
@@ -32,7 +32,7 @@ export class TheatersService {
     );
   }
 
-  async remove(id: string) {
-    return await this.theatersModel.deleteOne({ _id: id });
+  remove(id: string) {
+    return `This action removes a #${id} thater`;
   }
 }
