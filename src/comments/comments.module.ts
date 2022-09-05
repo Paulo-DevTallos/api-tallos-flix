4e5da41d11a205f132c4618e749d52a857309312
Paulo Dev Tallos <107865369+Paulo-DevTallos@users.noreply.github.com>
2022-09-05 15:05:57 -0300

    Revert "criando collection mongo db"
    This reverts commit bbb4bde185943aa1a693f275c5b03d845b2f81c1.

--- src/comments/comments.module.ts
+++ src/comments/comments.module.ts
@@ -1,14 +1,9 @@
 import { Module } from '@nestjs/common';
 import { CommentsService } from './comments.service';
 import { CommentsController } from './comments.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CommentSchema } from './entities/comment.entity';
 
 @Module({
-  imports: [
-    MongooseModule.forFeature([{ name: Comment.name, schema: CommentSchema }]),
-  ],
   controllers: [CommentsController],
-  providers: [CommentsService],
+  providers: [CommentsService]
 })
 export class CommentsModule {}
