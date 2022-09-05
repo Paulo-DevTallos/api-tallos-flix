d409b86449f67b5e1ad538de2dc7ddd1698880c2
Paulo Sergio <pauloserg861@gmail.com>
2022-09-05 15:17:07 -0300

    implementando authModule ao modulo de usuarios

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,10 +3,12 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User, UserSchema } from './entities/user.entity';
 import { MongooseModule } from '@nestjs/mongoose';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    AuthModule,
   ],
   controllers: [UsersController],
   providers: [UsersService],
