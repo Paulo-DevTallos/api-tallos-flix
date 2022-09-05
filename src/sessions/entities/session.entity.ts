f68cfbf9985b21058525ea907c6ef35402005bb1
Paulo Dev Tallos <107865369+Paulo-DevTallos@users.noreply.github.com>
2022-09-05 15:05:57 -0300

    Revert "criando entidade de sessions"
    This reverts commit 25ac641aff06d8316c8e2a3f6f00ffdcf5b849a5.

--- src/sessions/entities/session.entity.ts
+++ src/sessions/entities/session.entity.ts
@@ -1,14 +1,1 @@
-import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-
-export type SessionDocument = Session & Document;
-
-@Schema()
-export class Session {
-  @Prop()
-  user_id: string;
-
-  @Prop()
-  jwt: string;
-}
-
-export const SessionSchema = SchemaFactory.createForClass(Session);
+export class Session {}
