f14a1c4bef3207d8a8846a37b216b005ddefd0e7
Paulo Sergio <pauloserg861@gmail.com>
2022-09-06 14:44:39 -0300

    implementando jwtModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersModule } from 'src/users/users.module';
 import { LocalStrategy } from './strategy/local.strategy';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '24h' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy],
 })
