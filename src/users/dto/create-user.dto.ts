41b1405f587830d9941ed18fffb451b7a80e2ab6
Paulo Sergio <pauloserg861@gmail.com>
2022-09-06 12:01:34 -0300

    aplicando validacoes ao DTO de usuarios

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,14 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
   id?: string;
+
+  @IsNotEmpty()
   name: string;
+
+  @IsEmail()
   email: string;
+
+  @IsString()
   password: string;
 }
