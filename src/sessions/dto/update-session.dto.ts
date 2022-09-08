1a0686c9e30b86dcc2cbcfc8c8899f598c24ca3f
Paulo Sergio <pauloserg861@gmail.com>
2022-09-08 11:39:36 -0300

    implementando documentacao dos dtos

--- src/sessions/dto/update-session.dto.ts
+++ src/sessions/dto/update-session.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateSessionDto } from './create-session.dto';
 
-export class UpdateSessionDto extends PartialType(CreateSessionDto) {}
+export class UpdateSessionDto extends PartialType(CreateSessionDto) {
+  @ApiProperty({
+    // eslint-disable-next-line prettier/prettier
+    description: 'Os campos de atualização de sessão seguem o mesmo padrão do CreateSessionDto',
+  })
+  updateSession: CreateSessionDto;
+}
