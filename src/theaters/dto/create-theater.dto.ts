d85da4f9e205d2fe7c925afb97fb811412b3395b
Paulo Sergio <pauloserg861@gmail.com>
2022-09-08 11:53:44 -0300

    implementando documentacao dos dtos

--- src/theaters/dto/create-theater.dto.ts
+++ src/theaters/dto/create-theater.dto.ts
@@ -1,6 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { ITheaters } from '../interface/ITheaters';
 
 export class CreateTheaterDto {
+  @ApiProperty({
+    // eslint-disable-next-line prettier/prettier
+    description: 'Trás o código de identificação da sala de cinema',
+  })
   theatherId: number;
+
+  @ApiProperty({
+    // eslint-disable-next-line prettier/prettier
+    description: 'Agrupa os resultados de endereço e posicionamento geografico das salar',
+  })
   location: ITheaters;
 }
