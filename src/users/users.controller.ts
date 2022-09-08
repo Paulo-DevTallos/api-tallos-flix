a744aea42a978271dc269ac7e38131cc65fdd8d6
Paulo Sergio <pauloserg861@gmail.com>
2022-09-08 15:05:52 -0300

    implementando isPubluc e ApiBearer

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,13 +10,16 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { isPublic } from 'src/auth/decorators/is-public-decorator';
 
 @ApiTags('users')
+@ApiBearerAuth('JWT-auth')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @isPublic()
   @Post('POST')
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
