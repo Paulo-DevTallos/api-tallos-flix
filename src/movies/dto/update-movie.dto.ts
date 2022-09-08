b8f8acc01c0f53d814801b14f4b0ed1ade25f9b1
Paulo Sergio <pauloserg861@gmail.com>
2022-09-08 11:34:42 -0300

    implementando documntação de dtos

--- src/movies/dto/update-movie.dto.ts
+++ src/movies/dto/update-movie.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateMovieDto } from './create-movie.dto';
 
-export class UpdateMovieDto extends PartialType(CreateMovieDto) {}
+export class UpdateMovieDto extends PartialType(CreateMovieDto) {
+  @ApiProperty({
+    // eslint-disable-next-line prettier/prettier
+    description: 'Os campos de atualização de comentários seguem o mesmo padrão do CreateCommentsDto',
+  })
+  updateComments: CreateMovieDto;
+}
