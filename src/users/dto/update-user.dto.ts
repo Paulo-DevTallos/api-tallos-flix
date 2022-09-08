655a466e1ae25c00a6bd695f131bfa0cb2ab9b1b
Paulo Sergio <pauloserg861@gmail.com>
2022-09-08 10:39:57 -0300

    criando documentacao de update

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,11 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+  @ApiProperty({
+    // eslint-disable-next-line prettier/prettier
+    description: 'Os campos de atualização de comentários seguem o mesmo padrão do CreateUserDto',
+  })
+  updateComments: CreateUserDto;
+}
